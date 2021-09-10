fd4fddbf61224bf597c0086a111d6f0e729c27d6
London <london.rosemary@gmail.com>
2021-09-10 09:23:40 -0400

    Deliverable 3 complete

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({plant}) {
+  const [isInStock, setInStock] = useState(true)
+
+  function handleStockButton() {
+    setInStock(!isInStock);
+  }
+
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {isInStock ? (
+        <button onClick={handleStockButton} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleStockButton}>Out of Stock</button>
       )}
     </li>
   );
